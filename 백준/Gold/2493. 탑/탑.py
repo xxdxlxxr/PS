30acb54b566710e3d6746e1c60370b7de20429e2
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-03-15 09:04:08 +0900

    [Gold V] Title: 탑, Time: 752 ms, Memory: 168912 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,18 @@
+N = int(input())
+A = list(map(int, input().split()))
+stack, answer = [], []
+
+for i in range(N):
+    while stack:
+        if stack[-1][1] > A[i]:
+            answer.append(stack[-1][0] + 1)
+            break
+        else:
+            stack.pop()
+
+    if not stack:
+        answer.append(0)
+
+    stack.append([i, A[i]])
+
+print(" ".join(map(str, answer)))
