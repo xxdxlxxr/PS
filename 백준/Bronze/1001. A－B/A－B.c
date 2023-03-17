a7cbec2fbf2e0b234bbda0a279a5cd0422e67d92
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-03-18 00:18:23 +0900

    Update A－B.c

--- 백준/Bronze/1001. A－B/A－B.c
+++ 백준/Bronze/1001. A－B/A－B.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-	int a, b;
+	int A, B;
     
-	scanf("%d %d", &a, &b);
-	printf("%d\n", a - b);
+	scanf("%d %d", &A, &B);
+	printf("%d\n", A - B);
     
 	return 0;
 }
