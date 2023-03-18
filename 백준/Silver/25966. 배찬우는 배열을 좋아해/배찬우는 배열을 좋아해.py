9be66fe5d9d07fe20d82570937de7fbb2643e00d
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-03-18 11:02:22 +0900

    [Silver V] Title: 배찬우는 배열을 좋아해, Time: 3044 ms, Memory: 222140 KB -BaekjoonHub

---
+++ 백준/Silver/25966. 배찬우는 배열을 좋아해/배찬우는 배열을 좋아해.py
@@ -0,0 +1,18 @@
+N, M, q = map(int, input().split())
+answer = []
+
+for _ in range(N):
+    answer.append(list(map(int, input().split())))
+
+for _ in range(q):
+    query = list(map(int, input().split()))
+
+    if query[0]:
+        tmp = answer[query[1]]
+        answer[query[1]] = answer[query[2]]
+        answer[query[2]] = tmp
+    else:
+        answer[query[1]][query[2]] = query[3]
+
+for arr in answer:
+    print(*arr)
