4e9202beb6527a5305227d5021b90bf977175e5d
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-04-15 02:08:12 +0900

    [level 0] Title: 평행, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.py
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -1,9 +1,11 @@
 def solution(dots):
-    if (dots[0][0] - dots[1][0]) / (dots[0][1] - dots[1][1]) == (dots[2][0] - dots[3][0]) / (dots[2][1] - dots[3][1]):
-        return 1
-    elif (dots[0][0] - dots[2][0]) / (dots[0][1] - dots[2][1]) == (dots[1][0] - dots[3][0]) / (dots[1][1] - dots[3][1]):
-        return 1
-    if (dots[0][0] - dots[3][0]) / (dots[0][1] - dots[3][1]) == (dots[1][0] - dots[2][0]) / (dots[1][1] - dots[2][1]):
-        return 1
+    A, B, C, D = dots[0], dots[1], dots[2], dots[3]
     
-    return 0
+    if (A[0] - B[0]) / (A[1] - B[1]) == (C[0] - D[0]) / (C[1] - D[1]):
+        return 1
+    elif (A[0] - C[0]) / (A[1] - C[1]) == (B[0] - D[0]) / (B[1] - D[1]):
+        return 1
+    elif (A[0] - D[0]) / (A[1] - D[1]) == (B[0] - C[0]) / (B[1] - C[1]):
+        return 1
+    else:
+        return 0
