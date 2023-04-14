8a06979624f6e6a0da69116a543cee97a41d3393
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-04-14 11:26:13 +0900

    [Bronze III] Title: 노 땡스!, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27159. 노 땡스！/노 땡스！.py
@@ -0,0 +1,8 @@
+N, cards = int(input()), sorted(list(map(int, input().split())))
+answer = cards[0]
+
+for i in range(1, N):
+    if cards[i] - cards[i - 1] != 1:
+        answer += cards[i]
+
+print(answer)
