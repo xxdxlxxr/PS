2067cca87bd1ff675c95e5bee8da407c1429a08b
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-04-17 12:28:23 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,8 +1,2 @@
 def solution(numbers):
-    answer = 0
-    
-    for i in range(10):
-        if not numbers.count(i):
-            answer += i
-    
-    return answer
+    return sum(range(10)) - sum(numbers)
