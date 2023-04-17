c00dedc7ea70e42621ea2e85419c71d6c4726374
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-04-18 00:06:21 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.07 ms
+메모리: 10.1 MB, 시간: 0.07 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -2,12 +2,9 @@ def solution(n, words):
     store = [words[0]]
 
     for i in range(len(words) - 1):
-        if words[i][-1] != words[i + 1][0]:
+        if words[i][-1] != words[i + 1][0] or words[i + 1] in store:
             return [(i + 1) % n + 1, (i + 1) // n + 1]
         else:
-            if words[i + 1] in store:
-                return [(i + 1) % n + 1, (i + 1) // n + 1]
-            else:
-                store.append(words[i + 1])
+            store.append(words[i + 1])
 
     return [0, 0]
