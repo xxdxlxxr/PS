6959be9d4a560a4a37607e3ab561aba037fb31ef
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-04-18 00:04:02 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,13 @@
+def solution(n, words):
+    store = [words[0]]
+
+    for i in range(len(words) - 1):
+        if words[i][-1] != words[i + 1][0]:
+            return [(i + 1) % n + 1, (i + 1) // n + 1]
+        else:
+            if words[i + 1] in store:
+                return [(i + 1) % n + 1, (i + 1) // n + 1]
+            else:
+                store.append(words[i + 1])
+
+    return [0, 0]
