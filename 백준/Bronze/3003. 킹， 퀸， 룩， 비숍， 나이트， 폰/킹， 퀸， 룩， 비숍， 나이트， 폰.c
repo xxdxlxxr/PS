f273e7a6b71d3029e14eac1619937ac9a7929735
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-05-05 00:06:20 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int K, Q, R, B, N, P;
+    scanf("%d %d %d %d %d %d", &K, &Q, &R, &B, &N, &P);
+    
+    printf("%d %d %d %d %d %d", 1 - K, 1 - Q, 2 - R, 2 - B, 2 - N, 8 - P);
+}
