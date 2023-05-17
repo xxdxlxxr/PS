c4462124926a02cb4b675fb20f3eb703f55a5f7e
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-05-18 01:23:27 +0900

    [Bronze I] Title: 소인수분해, Time: 244 ms, Memory: 17996 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int N = sc.nextInt();
+	    
+	    while (N != 1) {
+	        for (int i = 2; i <= N; i++) {
+	            if (N % i == 0) {
+	                N /= i;
+	                System.out.println(i);
+	                break;
+	            }
+	        }
+	    }
+	}
+}
