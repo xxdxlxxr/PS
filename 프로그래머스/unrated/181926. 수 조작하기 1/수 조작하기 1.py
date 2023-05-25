807cb4b1ac26db9cbd6bcbb6c570f4ac8f4af30e
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-05-25 10:58:55 +0900

    [unrated] Title: 수 조작하기 1, Time: 2.74 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,12 +1,8 @@
 def solution(n, control):
     for alpha in control:
-        if alpha == 'w':
-            n += 1
-        elif alpha == 's':
-            n -= 1
-        elif alpha == 'd':
-            n += 10
-        else:
-            n -= 10
+        if alpha == 'w': n += 1
+        elif alpha == 's': n -= 1
+        elif alpha == 'd': n += 10
+        else: n -= 10
     
     return n
