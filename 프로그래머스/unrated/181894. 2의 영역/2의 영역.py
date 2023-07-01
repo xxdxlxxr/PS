93462e9c7f34ebeeec1007ae61e2c40c7752ae7b
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-07-01 11:44:29 +0900

    [unrated] Title: 2의 영역, Time: 0.60 ms, Memory: 15.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
@@ -1,12 +1,2 @@
 def solution(arr):
-    cnt = arr.count(2)
-
-    if cnt > 1:
-        if arr[-1] == 2:
-            return arr[arr.index(2):]
-        else:
-            return arr[arr.index(2):-arr[::-1].index(2)]
-    elif cnt == 1:
-        return [2]
-    else:
-        return [-1]
+    return arr[arr.index(2):len(arr) - arr[::-1].index(2)] if 2 in arr else [-1]
