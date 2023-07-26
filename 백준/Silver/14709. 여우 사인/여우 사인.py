904c93b78e572732d5ac56424567f6147ec90fdb
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-07-26 13:57:44 +0900

    [Silver V] Title: 여우 사인, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14709. 여우 사인/여우 사인.py
+++ 백준/Silver/14709. 여우 사인/여우 사인.py
@@ -1,4 +1,1 @@
-if int(input()) == 3 and sorted([sorted(list(map(int, input().split()))) for _ in range(3)]) == [[1, 3], [1, 4], [3, 4]]:
-    print('Wa-pa-pa-pa-pa-pa-pow!')
-else:
-    print('Woof-meow-tweet-squeek')
+print('Wa-pa-pa-pa-pa-pa-pow!' if int(input()) == 3 and sorted([sorted(list(map(int, input().split()))) for _ in range(3)]) == [[1, 3], [1, 4], [3, 4]] else 'Woof-meow-tweet-squeek')
