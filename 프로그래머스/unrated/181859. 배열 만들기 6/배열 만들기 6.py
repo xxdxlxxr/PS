7fa84c9c6d6174af5b911b84fefcac97c64e4d94
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-08-04 13:46:43 +0900

    [unrated] Title: 배열 만들기 6, Time: 11.96 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
@@ -2,13 +2,9 @@ def solution(arr):
     answer = []
     
     for i, num in enumerate(arr):
-        if not answer:
-            answer = [num]
-            continue
-            
-        if answer[-1] == num:
+        if answer and answer[-1] == num:
             answer.pop()
         else:
             answer.append(num)
     
-    return answer if answer else [-1]
+    return answer or [-1]
