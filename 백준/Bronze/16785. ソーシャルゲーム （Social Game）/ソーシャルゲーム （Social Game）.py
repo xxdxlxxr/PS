25b913355ae1b79377351762fdb4e62da64a83ce
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-10-03 13:02:31 +0900

    [Bronze III] Title: ソーシャルゲーム (Social Game), Time: 40 ms, Memory: 31256 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/16785. ソーシャルゲーム （Social Game）/ソーシャルゲーム （Social Game）.py
+++ 백준/Bronze/16785. ソーシャルゲーム （Social Game）/ソーシャルゲーム （Social Game）.py
@@ -1,3 +1,3 @@
 A, B, C = map(int, input().split())
 
-print(7 * (C // (7 * A + B)) + C % (7 * A + B) if B else C // A + (C % A != 0))
+print(C // (7 * A + B) * 7 + min(-(C % (7 * A + B) // -A), 7))
