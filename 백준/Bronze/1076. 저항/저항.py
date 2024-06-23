65484e9eebebe44373dfe46ab4a78b4929d8d34a
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2024-06-23 16:11:44 +0900

    [Bronze II] Title: 저항, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,2 @@
+color = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+print(int(str(color.index(input())) + str(color.index(input())) + str(color.index(input()) * '0')))
