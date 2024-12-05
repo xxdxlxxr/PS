220b35581c91dcacf3455289864bb9d22d7c1e9b
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2024-12-05 11:17:27 +0900

    [Bronze II] Title: 래환이의 여자친구 사귀기 대작전, Time: 76 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Bronze/32529. 래환이의 여자친구 사귀기 대작전/래환이의 여자친구 사귀기 대작전.py
+++ 백준/Bronze/32529. 래환이의 여자친구 사귀기 대작전/래환이의 여자친구 사귀기 대작전.py
@@ -1,11 +1,12 @@
 N, M = map(int, input().split())
 A = list(reversed(list(map(int, input().split()))))
+total = 0
 
-for i in range(N - 1):
-    A[i + 1] += A[i]
+for i in range(N):
+    total += A[i]
 
-    if A[i + 1] >= M:
-        print(N - i - 1)
+    if total >= M:
+        print(N - i)
         break
 else:
     print(-1)
