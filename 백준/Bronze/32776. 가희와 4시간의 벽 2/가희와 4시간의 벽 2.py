4c4c7b21747e22bd48af509a6e18231b2d74a635
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2024-12-08 14:49:50 +0900

    [Bronze IV] Title: 가희와 4시간의 벽 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/32776. 가희와 4시간의 벽 2/가희와 4시간의 벽 2.py
+++ 백준/Bronze/32776. 가희와 4시간의 벽 2/가희와 4시간의 벽 2.py
@@ -1,2 +1,2 @@
 Sab = int(input())
-print('high speed rail' if Sab <= 240 or Sab <= sum(map(int, input().split())) else 'flight')
+print('high speed rail' if Sab <= max(240, sum(map(int, input().split()))) else 'flight')
