3479fe8617950e2037d7af6f5591d403148360f9
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2025-02-10 12:43:20 +0900

    [Bronze V] Title: Football Scoring, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.py
@@ -0,0 +1,2 @@
+score, visiting, home = [6, 3, 2, 1, 2], list(map(int, input().split())), list(map(int, input().split()))
+print(sum(score[i] * visiting[i] for i in range(5)), sum(score[i] * home[i] for i in range(5)))
