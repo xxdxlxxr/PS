6d4fb7c1d4e45a086d197510067459e53f2714fc
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2025-09-15 19:39:35 +0900

    [Bronze II] Title: 방 배정, Time: 60 ms, Memory: 34536 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/13304. 방 배정/방 배정.py
@@ -0,0 +1,23 @@
+import math
+
+N, K = map(int, input().split())
+student, answer = 5 * [0], 0
+
+for _ in range(N):
+    S, Y = map(int, input().split())
+
+    if Y == 1 or Y == 2:
+        student[0] += 1
+    elif S == 0 and (Y == 3 or Y == 4):
+        student[1] += 1
+    elif S == 1 and (Y == 3 or Y == 4):
+        student[2] += 1
+    elif S == 0 and (Y == 5 or Y == 6):
+        student[3] += 1
+    else:
+        student[4] += 1
+
+for num in student:
+    answer += math.ceil(num / K)
+
+print(answer)
