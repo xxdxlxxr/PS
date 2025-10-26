6da1014d0cd6a4f0ad6a9dcd97adef0f6c93d09d
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2025-10-26 12:25:48 +0900

    [Silver V] Title: 팩토리얼 분해, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.py
@@ -0,0 +1,13 @@
+N, tmp, total = int(input()), [1, 1], 0
+
+for i in range(2, 21):
+    tmp.append(i * tmp[i - 1])
+
+for i in range(20, -1, -1):
+    if total + tmp[i] < N:
+        total += tmp[i]
+    elif total + tmp[i] == N:
+        print('YES')
+        exit(0)
+else:
+    print('NO')
