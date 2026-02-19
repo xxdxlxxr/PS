293f20172ab6d538b6743574e3dd45537bafa23c
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2026-02-19 16:28:16 +0900

    [Silver III] Title: 선물, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1166. 선물/선물.py
@@ -0,0 +1,12 @@
+N, L, W, H = map(int, input().split())
+start, end = 0, max(L, W, H)
+
+for _ in range(100):
+    mid = (start + end) / 2
+
+    if (L // mid) * (W // mid) * (H // mid) >= N:
+        start = mid
+    else:
+        end = mid
+
+print('%.10f' % end)
