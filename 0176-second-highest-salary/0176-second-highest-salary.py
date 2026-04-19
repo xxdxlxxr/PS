9dcc89f49582e0b581017c8569515868d39d3cb5
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2026-04-19 12:49:51 +0900

    Delete 0176-second-highest-salary directory

--- 0176-second-highest-salary/0176-second-highest-salary.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
-    salaries = sorted(set(employee['salary']))
-    
-    if len(salaries) < 2:
-        return pd.DataFrame({'SecondHighestSalary': [None]})
-    
-    return pd.DataFrame({'SecondHighestSalary': [salaries[-2]]})
